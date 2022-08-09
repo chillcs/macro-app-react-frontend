5d9a70f3948744a19cac4315c43fb4e90ddd4881
Curtis <curtis@cstudio.ca>
2022-08-08 18:58:02 -0700

    app height

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 const Dashboard = () => {
-	const customStyles = {
-		height: 'calc(var(--vh, 1vh) * 100)',
+	const [height, setHeight] = useState(window.innerHeight);
+	const updateHeight = () => {
+		setHeight(window.innerHeight);
 	};
+
+	useEffect(() => {
+		window.addEventListener('resize', updateHeight);
+		return () => window.removeEventListener('resize', updateHeight);
+	}, []);
+
+	const maxHeight = {
+		height: height,
+	};
+
+	const bodyHeight = {
+		height: maxHeight.height - 125,
+	};
+
+	console.log(maxHeight.height);
 
 	return (
 		<>
-			<div className="dashboard" style={customStyles}>
-				Dashboard
+			<div className="dashboard" style={maxHeight}>
+				<div className="header">Macro</div>
+				<div className="main" style={bodyHeight}>
+					<div className="test">test</div>
+				</div>
+				<div className="footer">
+					<div className="tab">Tracker</div>
+					<div className="tab">Library</div>
+					<div className="tab">Settings</div>
+				</div>
 			</div>
 		</>
 	);
