cf4f53071f0b4012816bf9a1f1f5df3593b9480f
Curtis <curtis@cstudio.ca>
2022-08-08 20:21:18 -0700

    tabs

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,38 +1,60 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import Tracker from '../Dashboard/Tracker/index';
+import Library from '../Dashboard/Library/index';
+import Settings from '../Dashboard/Settings/index';
 
 const Dashboard = () => {
+	// Set heights ---
 	const [height, setHeight] = useState(window.innerHeight);
 	const updateHeight = () => {
 		setHeight(window.innerHeight);
 	};
-
 	useEffect(() => {
 		window.addEventListener('resize', updateHeight);
 		return () => window.removeEventListener('resize', updateHeight);
 	}, []);
-
 	const maxHeight = {
 		height: height,
 	};
-
+	const headerHeight = {
+		height: 75,
+	};
+	const footerHeight = {
+		height: 50,
+	};
 	const bodyHeight = {
-		height: maxHeight.height - 125,
+		height: maxHeight.height - headerHeight.height - footerHeight.height,
 	};
 
-	console.log(maxHeight.height);
+	// Toggle tabs ---
+	const [active, setActive] = useState(0);
+	function openTab(event) {
+		const target = event.currentTarget.id;
+		setActive((active) => (active === target ? active : target));
+	}
 
 	return (
 		<>
 			<div className="dashboard" style={maxHeight}>
-				<div className="header">Macro</div>
-				<div className="main" style={bodyHeight}>
-					<div className="test">test</div>
+				<div className="header" style={headerHeight}>
+					Macro
 				</div>
-				<div className="footer">
-					<div className="tab">Tracker</div>
-					<div className="tab">Library</div>
-					<div className="tab">Settings</div>
+				<div className="main" style={bodyHeight}>
+					{parseInt(active) === 0 && <Tracker />}
+					{parseInt(active) === 1 && <Library />}
+					{parseInt(active) === 2 && <Settings />}
+				</div>
+				<div className="footer" style={footerHeight}>
+					<div className="tab" id={0} onClick={openTab}>
+						Tracker
+					</div>
+					<div className="tab" id={1} onClick={openTab}>
+						Library
+					</div>
+					<div className="tab" id={2} onClick={openTab}>
+						Settings
+					</div>
 				</div>
 			</div>
 		</>
